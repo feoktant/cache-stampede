a8c6fb163dc19e982171a2cf06c92de8205bc99b
Anton Feoktistov <anton.feoktistov@gmail.com>
2020-07-18 00:36:42 +0300

    Extracted trait with caching algorithms

--- src/main/scala/io/feoktant/DeepThought.scala
+++ src/main/scala/io/feoktant/DeepThought.scala
@@ -8,14 +8,15 @@ import akka.util.Timeout
 import io.feoktant.akkacache.Cache
 import org.slf4j.{Logger, LoggerFactory}
 import scredis.Redis
-import scredis.serialization.{IntReader, Reader, Writer}
+import scredis.serialization.IntReader
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Random
 
-class DeepThought(redis: Redis,
-                  system: ActorSystem)(implicit val ec: ExecutionContext) {
+class DeepThought(val redis: Redis,
+                  system: ActorSystem)(implicit val ec: ExecutionContext)
+  extends CachingAlgorithms {
 
   private val log: Logger = LoggerFactory.getLogger(classOf[DeepThought])
   private val random = Random
@@ -40,96 +41,10 @@ class DeepThought(redis: Redis,
     }
   }
 
-  private def naiveCache[T](key: String, ttl: FiniteDuration)
-                           (recomputeValue: => Future[T])
-                           (implicit
-                            reader: Reader[T],
-                            writer: Writer[T]): Future[T] = {
-    for {
-      cached <- redis.get[T](key)
-      value  <- cached match {
-        case Some(v) =>
-          log.debug("Cached key {}", key)
-          Future.successful(v)
-
-        case None    =>
-          log.warn("Cache miss for key {}", key)
-          for {
-            value <- recomputeValue
-            _     <- redis.pSetEX(key, value, ttl.toMillis)
-          } yield value
-      }
-    } yield value
-  }
-
   def cachedXFetchedTheAnswerOfLife: Future[Int] =
     xFetch("xfetch:the_answer_of_life", ttl, beta = 0.1) {
       computeTheAnswerOfLife
     }
-
-  /**
-   * Cache stampede algorithm implementation with Redis.
-   * Canonical pseudo-code:
-   *
-   * function x-fetch(key, ttl, beta=1) {
-   *   value, delta, expiry ← cache_read(key)
-   *   if (!value || time() − delta * beta * log(rand(0,1)) ≥ expiry) {
-   *     start ← time()
-   *     value ← recompute_value()
-   *     delta ← time() – start
-   *     cache_write(key, (value, delta), ttl)
-   *   }
-   *   return value
-   * }
-   *
-   * @see http://cseweb.ucsd.edu/~avattani/papers/cache_stampede.pdf
-   * @see https://www.slideshare.net/RedisLabs/redisconf17-internet-archive-preventing-cache-stampede-with-redis-and-xfetch
-   */
-  private def xFetch[T](key: String, ttl: FiniteDuration, beta: Double = 1.0D)
-                       (recomputeValue: => Future[T])
-                       (implicit
-                        redisTuple2GenericLongReader: Reader[(T, Long)],
-                        redisTuple2GenericLongWriter: Writer[(T, Long)]): Future[T] = {
-    import System.{currentTimeMillis => time}
-
-    def shouldRecompute(delta: Long, expiryTtl: Long): Boolean =
-      (delta * beta * Math.log(random.nextDouble())).abs >= expiryTtl
-
-    val cachedF = redis.get[(T, Long)](key)
-    val ttlMsF = redis.pTtl(key)
-
-    def recompute(): Future[T] = {
-      val start = time()
-      recomputeValue.map { value =>
-        val delta = time() - start
-        redis.pSetEX(key, (value, delta), ttl.toMillis)
-        log.debug("Recompute key {} took {}ms", key, delta)
-        value
-      }
-    }
-
-    for {
-      cached    <- cachedF
-      expiryTtl <- ttlMsF
-      value     <- (cached, expiryTtl) match {
-        case (Some((value, delta)), Right(expiryTtl)) =>
-          if (shouldRecompute(delta, expiryTtl)) {
-            log.info("Should recompute key {}, remaining ttl {}ms", key, expiryTtl)
-            recompute() // side effect
-          }
-          log.debug("Cached key {}", key)
-          Future.successful(value)
-
-        case (Some((value, _)), Left(true)) =>
-          log.warn("xFetch key {} with no TTL, skipping update", key)
-          Future.successful(value)
-
-        case _ =>
-          log.warn("Cache miss for key {}", key)
-          recompute()
-      }
-    } yield value
-  }
 
   def recomputeBlockingAkka: Future[Int] = {
     implicit val timeout: Timeout = Timeout(15, TimeUnit.SECONDS)
